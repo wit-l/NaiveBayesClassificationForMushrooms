9a4b80a6d67cc39c406adfec7fd98eb06d733646
WittyCo <witty.ysl@gmail.com>
2024-04-30 17:12:09 +0800

    wrap function

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import pandas as pd
 import pickle
 from train import train_model
+from typing import List, Tuple
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import LabelEncoder
+
+
+def load_encoders_model(
+    label_encoders_file: str, model_file: str
+) -> Tuple[List[LabelEncoder], GaussianNB]:
+    with open(label_encoders_file, "rb") as les_, open(model_file, "rb") as model_:
+        les = pickle.load(les_)
+        model = pickle.load(model_)
+    return les, model
+
 
 if __name__ == "__main__":
     # read column names
     file = "mushrooms.csv"
     columns = []
-    # TODO: The exception should be handled on the outside function
     try:
         with open(file, "r") as f:
             columns = f.readline().strip().split(",")[1:]
@@ -17,30 +29,11 @@ if __name__ == "__main__":
         print("发生预期外的错误, 原因：", e.__cause__)
         exit(-1)
 
-    # get encoders from file
-    file = "encoders.txt"
     try:
-        with open(file, "rb") as f:
-            labelEncoders = pickle.load(f)
+        labelEncoders, nb = load_encoders_model("encoders.txt", "model.txt")
     except FileNotFoundError:
-        # print("Can not open %s. Training the model first." % (file))
         train_model()
-        with open(file, "rb") as f:
-            labelEncoders = pickle.load(f)
-    except Exception as e:
-        print("发生预期外的错误, 原因：", e.__cause__)
-        exit(-1)
-
-    # read file to get model object
-    file = "model.txt"
-    try:
-        with open(file, "rb") as f:
-            nb = pickle.load(f)
-    except FileNotFoundError:
-        # print("Can not open %s. Training the model first." % (file))
-        train_model()
-        with open(file, "rb") as f:
-            nb = pickle.load(f)
+        labelEncoders, nb = load_encoders_model("encoders.txt", "model.txt")
     except Exception as e:
         print("发生预期外的错误, 原因：", e.__cause__)
         exit(-1)
@@ -52,15 +45,15 @@ if __name__ == "__main__":
         "请输入蘑菇的各个特征值(按照以上特征的顺序输入，并用','分隔各个特征的值)："
     )
 
-    X_te = pd.DataFrame([user_in_str.split(",")], columns=columns)
+    X = pd.DataFrame([user_in_str.split(",")], columns=columns)
     # print(X_te)
 
     i = 1
-    for col in X_te.columns:
-        X_te[col] = labelEncoders[i].transform(X_te[col])
+    for col in X.columns:
+        X[col] = labelEncoders[i].transform(X[col])
         i += 1
 
-    y_pred = nb.predict(X_te)[0]
+    y_pred = nb.predict(X)[0]
     if y_pred == 1:
         print("有毒")
     elif y_pred == 0:
