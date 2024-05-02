79d6c2cb453ea4dd9ac259379ff9237877167bba
WittyCo <witty.ysl@gmail.com>
2024-05-03 02:38:58 +0800

    Implement input/output info on the browser.

--- main.py
+++ main.py
@@ -20,61 +20,47 @@ def load_encoders_model(
 app = Flask(__name__)
 
 
-def process_input(user_input) -> str:
-    return "result of handling:" + user_input
-
-
-@app.route("/", methods=["GET", "POST"])
-def home():
-    if request.method == "POST":
-        user_input = request.form.get("user_input")
-        result = process_input(user_input)
-        return result
-    return render_template("form.html", columns=columns)
-
-
-if __name__ == "__main__":
-    app.run()
-    # read column names
-    file = "mushrooms.csv"
-    columns = []
-    try:
-        with open(file, "r") as f:
-            columns = f.readline().strip().split(",")[1:]
-    except FileNotFoundError:
-        print("Can not open %s." % (file))
-        exit(-1)
-    except Exception as e:
-        print("发生预期外的错误, 原因：", e.__cause__)
-        exit(-1)
-
+def process_input(user_input: str, columns: list[str]) -> str:
     try:
         labelEncoders, nb = load_encoders_model("encoders.txt", "model.txt")
     except FileNotFoundError:
         train_model()
         labelEncoders, nb = load_encoders_model("encoders.txt", "model.txt")
     except Exception as e:
-        print("发生预期外的错误, 原因：", e.__cause__)
-        exit(-1)
-
-    for i in columns:
-        print(i, end=" ")
-    print("\n")
-    user_in_str = input(
-        "请输入蘑菇的各个特征值(按照以上特征的顺序输入，并用','分隔各个特征的值)："
-    )
-
-    X = pd.DataFrame([user_in_str.split(",")], columns=columns)
-
+        return "发生预期外的错误, 原因：" + str(e.__cause__)
+    X = pd.DataFrame([user_input.split(",")], columns=columns)
     i = 1
     for col in X.columns:
         X[col] = labelEncoders[i].transform(X[col])
         i += 1
-
     y_pred = nb.predict(X)[0]
+    result = ""
     if y_pred == 1:
-        print("有毒")
+        result = "有毒"
     elif y_pred == 0:
-        print("无毒")
-    else:
-        print("error")
+        result = "无毒"
+    return "预测结果：" + result
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    # read column names
+    file = "mushrooms.csv"
+    columns = []
+
+    try:
+        with open(file, "r") as f:
+            columns = f.readline().strip().split(",")[1:]
+    except FileNotFoundError:
+        return "Can not open " + file
+    except Exception as e:
+        return "发生预期外的错误, 原因：" + str(e.__cause__)
+    if request.method == "POST":
+        user_input = request.form.get("user_input")
+        result = process_input(str(user_input), columns)
+        return result
+    return render_template("form.html", columns=columns)
+
+
+if __name__ == "__main__":
+    app.run()
