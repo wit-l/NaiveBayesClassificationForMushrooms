881ea701269f3b2d5c07fdb1670433632db9639d
WittyCo <witty.ysl@gmail.com>
2024-06-11 19:34:52 +0800

    Update

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #!/opt/anaconda3/envs/sklearn/bin/python3
+from sys import excepthook
 import pandas as pd
 import pickle
 from typing import List, Tuple
 from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import LabelEncoder
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, send_from_directory
 from train import train_model
 
 
@@ -22,6 +23,8 @@ accuracy_score = 0.0
 precision_score = 0.0
 recall_score = 0.0
 columns = []
+download_result_file = False
+result = ""
 
 
 def process_input(user_input: str, columns: list[str]) -> str:
@@ -44,7 +47,9 @@ def process_input(user_input: str, columns: list[str]) -> str:
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    global result
     result = ""
+    global download_result_file
     global columns
     if columns.__len__() == 0:
         # read column names
@@ -52,6 +57,7 @@ def index():
         try:
             with open(file, "r") as f:
                 columns = f.readline().strip().split(",")[1:]
+            # raise Exception
         except FileNotFoundError:
             result = "Can not open " + file + "!!!"
         except Exception as e:
@@ -60,15 +66,18 @@ def index():
     if request.method == "POST":
         user_input = request.form["sample_text"]
         file = request.files["sample_file"]
-        # 输入为空，或者未成功读取特征标签(result中含有报错信息)
-        # if user_input == "" or result != "":
-        #     return redirect("/")
 
         if file.filename != "":
-            with open("result.txt", "w", encoding="utf-8") as result_file:
+            download_result_file = True
+            with open("./result.txt", "w", encoding="utf-8") as result_file:
                 for line in file.stream:
                     # print(line.decode().strip())
-                    result = process_input(line.decode().strip(), columns)
+                    try:
+                        result = process_input(line.decode().strip(), columns)
+                    except ValueError:
+                        result = "输入格式不正确，请严格按照以上顺序输入完整！！！"
+                    except Exception as e:
+                        result = "发生预期外的错误, 原因：" + str(e.__doc__) + "！！！"
                     if result == 0:
                         result = str("无毒")
                     elif result == 1:
@@ -76,6 +85,14 @@ def index():
                     result_file.write(result)
                     result_file.write("\n")
                     # print(result)
+            if user_input == "":
+                return render_template(
+                    "form.html",
+                    columns=columns,
+                    accuracy_score=0,
+                    result="",
+                    download_result_file=download_result_file,
+                )
 
         try:
             result = process_input(str(user_input), columns)
@@ -91,17 +108,31 @@ def index():
         precision_score=precision_score,
         recall_score=recall_score,
         result=result,
+        download_result_file=download_result_file,
     )
 
 
 @app.route("/train", methods=["POST"])
 def invoke_train():
     try:
-        global accuracy_score, precision_score, recall_score
+        global accuracy_score, precision_score, recall_score, result
         accuracy_score, precision_score, recall_score = train_model()
-        return redirect("/")
+        return render_template(
+            "form.html",
+            columns=columns,
+            result=result,
+            download_result_file=download_result_file,
+            accuracy_score=accuracy_score,
+            precision_score=precision_score,
+            recall_score=recall_score,
+        )
     except Exception as e:
         return "发生预期外的错误, 原因：" + str(e.__doc__) + "!!!"
+
+
+@app.route("/download/<filename>", methods=["GET"])
+def download_file(filename):
+    return send_from_directory("./", filename, as_attachment=True)
 
 
 if __name__ == "__main__":
